c13d1e67d48a4fb4bbe9926b9f59e23d92cb9d34
TartejBrothers <taranjeetsinghbedi2@gmail.com>
2023-09-01 20:23:52 +0530

    changes

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 var flag = 0;
 function showmenu() {
-  if (flag == 0) {
-    document.getElementById("menu").style.display = "block";
-    flag = 1;
+  if (window.matchMedia("(max-width: 1000px)").matches) {
+    if (flag == 0) {
+      document.getElementById("menu").style.display = "block";
+      flag = 1;
+    } else {
+      document.getElementById("menu").style.display = "none";
+      flag = 0;
+    }
   } else {
     document.getElementById("menu").style.display = "none";
-    flag = 0;
   }
 }
+var onresize = function (e) {
+  document.getElementById("menu").style.display = "none";
+};
+window.addEventListener("resize", onresize);
